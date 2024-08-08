9fecca3fe14292ac1b630e0924de8dc9265720c4
suna255 <sunamammedova93@gmail.com>
2024-08-08 20:05:52 +0400

    salammmm

---
+++ tapsiriqqqqqqqqqq.py
@@ -0,0 +1,11 @@
+a=[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+b=[14,24,34,44,54,64,74,84,94,104,114,124,134,144]
+c=['alma','hello',True,55,'python',44,False]
+c[0]='salam'
+print(c)
+c[-1]=11
+print(c)
+a[-4:]= ['s','o','n','a']
+print(a)
+b[-3:]=['salam','hello','hallo']
+print(b)
