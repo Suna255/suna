d7e91de740a9365450e8a445e1854fe85cccd077
suna255 <sunamammedova93@gmail.com>
2024-08-12 19:52:10 +0400

    sunaaaaa

---
+++ lesson8.py
@@ -0,0 +1,14 @@
+a=['hello','salam','hallo']
+a.insert(3,'yoxx')
+#print(a)
+#a.append('yox')
+print(a)
+a.remove('hello')
+#print(a)
+#a.pop(2)
+print(a)
+#a.pop()
+print(a)
+#del a[]
+a.clear ()
+print(a)
