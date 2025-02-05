09b09b7ec0494efc91543a0b1edf8c97eaed1e33
suna255 <sunamammedova93@gmail.com>
2025-02-05 22:59:09 +0400

    sunaaa

---
+++ fiqur code.py
@@ -0,0 +1,23 @@
+from turtle import *
+import turtle as tur
+import random
+import time
+
+t=tur.Turtle()
+tur.title("clcoding.com")
+tur.speed(8)
+
+tur.bgcolor("black")
+tur.color("red")
+tur.pensize(6)
+
+tur.left(60)
+tur.fd(50)
+tur.left(15)
+tur.circle(100,90)
+tur.fd(30)
+tur.pensize(10)
+tur.penup()
+tur.right(90)
+tur.fd(20)
+tur.pendown()
